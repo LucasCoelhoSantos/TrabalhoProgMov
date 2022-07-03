3267efd366ba0c558435dbc74cc4360be6bed17e
Lucas de Carvalho Vilela Welter <61335851+boltwelter123@users.noreply.github.com>
2022-07-02 21:35:31 -0300

    Atualização do layout

--- UFMSQuizz/app/src/main/java/com/example/ufmsquizz/DBHelper.java
+++ UFMSQuizz/app/src/main/java/com/example/ufmsquizz/DBHelper.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
@@ -41,6 +42,42 @@ public class DBHelper extends SQLiteOpenHelper {
             return true;
         }
     }
+<<<<<<< Updated upstream
+=======
+    // Provavelmente tá com bug
+
+    public String salvarFoto( String fotoPerfil, String email){
+        SQLiteDatabase myDB = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("fotoPerfil",fotoPerfil);
+        String sql = "INSERT OR REPLACE INTO users (fotoPerfil) VALUES("+contentValues+") WHERE email ="+email;
+        myDB.execSQL(sql);
+        String fotoTeste = "SELECT fotoPerfil FROM users WHERE email ="+email;
+        return  fotoTeste;
+        //long result = myDB.insert("users",fotoPerfil,contentValues);
+         //if(result==-1){
+
+    }
+    //Provavelmente tá com bug
+    public Boolean checarFoto(String email){
+        SQLiteDatabase myDB = this.getWritableDatabase();
+        String fotoNoBd = String.valueOf(myDB.rawQuery("select fotoPerfil from users where email = ?",new String[]{email}));
+        if(fotoNoBd.equals("")||fotoNoBd==null){
+            return false;
+        }
+        else {
+            return true;
+        }
+
+    }
+
+    public String retornarFotoPerfil(String email){
+        SQLiteDatabase myDB = this.getWritableDatabase();
+        String fotoNoBd = String.valueOf(myDB.rawQuery("select fotoPerfil from users where email = ?",new String[]{email}));
+
+        return fotoNoBd;
+    }
+>>>>>>> Stashed changes
 
     public Boolean checkUser(String email){
         SQLiteDatabase myDB = this.getWritableDatabase();
--- UFMSQuizz/app/src/main/java/com/example/ufmsquizz/quizHomeActivity.java
+++ UFMSQuizz/app/src/main/java/com/example/ufmsquizz/quizHomeActivity.java
@@ -10,5 +10,36 @@ public class quizHomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_home);
+<<<<<<< Updated upstream
+=======
+
+        btnQuiz1 = (ImageButton)findViewById(R.id.btnQuiz1);
+        btnQuiz2 = (ImageButton)findViewById(R.id.btnQuiz2);
+        btnPerfil = (FloatingActionButton)findViewById(R.id.btnPerfil);
+
+        btnPerfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),PerfilActivity.class);
+                startActivity(intent );
+            }
+        });
+        btnQuiz1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),TelaQuiz.class);
+                startActivity(intent);
+            }
+        });
+        btnQuiz2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),TelaQuiz.class);
+                startActivity(intent);
+            }
+        });
+
+
+>>>>>>> Stashed changes
     }
 }
