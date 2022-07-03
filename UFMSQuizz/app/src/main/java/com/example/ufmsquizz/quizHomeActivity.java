6f7bc2f57a8713967762ded499c9ba3da2c5c36c
Lucas de Carvalho Vilela Welter <61335851+boltwelter123@users.noreply.github.com>
2022-07-03 00:01:20 -0300

    Alterações do layout
    Alterações

--- UFMSQuizz/app/src/main/java/com/example/ufmsquizz/quizHomeActivity.java
+++ UFMSQuizz/app/src/main/java/com/example/ufmsquizz/quizHomeActivity.java
@@ -6,20 +6,36 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class quizHomeActivity extends AppCompatActivity {
-    ImageButton btnQuiz1,btnQuiz2;
+    ImageView btnQuiz1,btnQuiz2;
     FloatingActionButton btnPerfil;
+
+    protected static int quizInt;
+
+    public static int GetQuizInt() {
+        return quizInt;
+    }
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_home);
 
+<<<<<<< Updated upstream
         btnQuiz1 = (ImageButton)findViewById(R.id.btnQuiz1);
         btnQuiz2 = (ImageButton)findViewById(R.id.btnQuiz2);
+=======
+
+        btnQuiz1 = (ImageView) findViewById(R.id.btnQuiz1);
+        btnQuiz2 = (ImageView) findViewById(R.id.btnQuiz2);
+>>>>>>> Stashed changes
         btnPerfil = (FloatingActionButton)findViewById(R.id.btnPerfil);
 
         btnPerfil.setOnClickListener(new View.OnClickListener() {
@@ -29,6 +45,27 @@ public class quizHomeActivity extends AppCompatActivity {
                 startActivity(intent );
             }
         });
+<<<<<<< Updated upstream
+=======
+        btnQuiz1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                quizInt=1;
+                Intent intent = new Intent(getApplicationContext(),TelaQuiz.class);
+                startActivity(intent);
+            }
+        });
+        btnQuiz2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                quizInt=2;
+                Intent intent = new Intent(getApplicationContext(),TelaQuiz.class);
+                startActivity(intent);
+            }
+        });
+
+
+>>>>>>> Stashed changes
 
     }
 }
